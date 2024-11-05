3bd221be48e6855a0cef8ab5c99be45b01778a49
perfectionless <95019743+perfectionless@users.noreply.github.com>
2024-11-06 06:18:27 +0800

    Added door opening

--- Assets/Script/PlayerCamera.cs
+++ Assets/Script/PlayerCamera.cs
@@ -19,14 +19,14 @@ public class PlayerCamera : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.deltaTime;
-        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.deltaTime;
+        float mouseX = Input.GetAxis("Mouse X") * mouseSens * Time.fixedDeltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSens * Time.fixedDeltaTime;
 
         xRotation -= mouseY;
         xRotation = Mathf.Clamp(xRotation, -90f, 90f);
 
         transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
         playerBody.Rotate(Vector3.up * mouseX);
-        
     }
+
 }
